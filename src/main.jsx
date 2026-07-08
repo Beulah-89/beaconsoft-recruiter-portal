024417a9cfeb38a95c92e38071e4130e0fddbea0
Beulah-89 <info@beaconsoft-it.com>
2026-07-09 01:28:56 +0530

    Wrap RecruiterPortal in BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import RecruiterPortal from './RecruiterPortal.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RecruiterPortal />
+    <BrowserRouter>
+      <RecruiterPortal />
+    </BrowserRouter>
   </React.StrictMode>
 );
